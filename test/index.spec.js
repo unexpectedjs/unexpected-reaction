0443bcb77e40e12b84f7991e1584c78552600a59
Sune Simonsen <sune@we-knowhow.dk>
2018-04-10 20:16:28 +0200

    Test that we can satisfy against a conditional tree.

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 import expect from "./unexpected-with-plugins";
+import { mount, Simulate } from "react-dom-testing";
 
 class Hello extends Component {
   render() {
@@ -30,6 +31,33 @@ Stateless.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string
 };
+
+class Toggle extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checked: false
+    };
+
+    this.onClick = () => {
+      this.setState(state => ({
+        checked: !state.checked
+      }));
+    };
+  }
+
+  render() {
+    const { checked } = this.state;
+
+    return (
+      <div onClick={this.onClick}>
+        {checked && <span style={{ color: "green" }}>TRUE</span>}
+        {!checked && <span style={{ color: "red" }}>FALSE</span>}
+      </div>
+    );
+  }
+}
 
 describe("unexpected-reaction", () => {
   describe("ReactElement", () => {
@@ -86,6 +114,30 @@ describe("unexpected-reaction", () => {
             </div>
           );
         }, "with error matching snapshot");
+      });
+
+      describe("on conditional tree", () => {
+        it("mounts the given ReactElement and satisfies the subject against it", () => {
+          const mounted = mount(<Toggle />);
+
+          expect(
+            mounted,
+            "to satisfy",
+            <div>
+              <span>FALSE</span>
+            </div>
+          );
+
+          Simulate.click(mounted);
+
+          expect(
+            mounted,
+            "to satisfy",
+            <div>
+              <span>TRUE</span>
+            </div>
+          );
+        });
       });
     });
 
